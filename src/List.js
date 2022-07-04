b1ddaea8e618def06e3673b355caa61124607a55
Mohamed Fouad <mofouad001@gmail.com>
2022-07-04 22:00:33 +0200

    first commit

---
+++ src/List.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+const List = ({people}) => {
+    return (  
+    <>
+        {people.map((person) => {
+            const {id,name,age,image} = person;
+            return(
+                <article className="person" key={id}>
+                    <img src={image} alt={name}/>
+                    <div>
+                        <h4>{name}</h4>
+                        <p>{age}</p>
+                    </div>
+                </article>
+            )
+        })}
+    </>
+    );
+    
+}
+
+export default List;
